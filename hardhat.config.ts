cf87a3cfc932e9c55a4e7b49f1ef7b53ed8eeae1
tuhoangwkm <tuhoang@wakumo.vn>
2022-12-19 09:18:28 +0700

    feat: solidity with hardhat

---
+++ hardhat.config.ts
@@ -0,0 +1,25 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+
+// Go to https://www.alchemyapi.io, sign up, create
+// a new App in its dashboard, and replace "KEY" with its key
+const ALCHEMY_API_KEY = "EGHsNy7EJXku-ntRPwSM2OKgQBS5U6-t";
+
+// Replace this private key with your Goerli account private key
+// To export your private key from Metamask, open Metamask and
+// go to Account Details > Export Private Key
+// Beware: NEVER put real Ether into testing accounts
+const GOERLI_PRIVATE_KEY =
+  "d901ae72a7be2ca3cc7e6f08f427e4cd4159881acd0a8c3fd66eac35396a4742";
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.17",
+  networks: {
+    goerli: {
+      url: `https://eth-goerli.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
+      accounts: [GOERLI_PRIVATE_KEY],
+    },
+  },
+};
+
+export default config;
---
+++ scripts/deploy.ts
@@ -0,0 +1,39 @@
+import { ethers } from "hardhat";
+
+async function main() {
+  // const currentTimestampInSeconds = Math.round(Date.now() / 1000);
+  // const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
+  // const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+
+  // const lockedAmount = ethers.utils.parseEther("1");
+
+  // const Lock = await ethers.getContractFactory("Lock");
+  // const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+
+  // await lock.deployed();
+
+  // console.log(
+  //   `Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
+  // );
+
+  const [deployer] = await ethers.getSigners();
+
+  console.log("Deploying contracts with the account:", deployer.address);
+
+  console.log("Account balance:", (await deployer.getBalance()).toString());
+
+  const Token = await ethers.getContractFactory("Token");
+  const token = await Token.deploy();
+
+  console.log("Token address:", token.address);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+    // process.exitCode = 1;
+  });
